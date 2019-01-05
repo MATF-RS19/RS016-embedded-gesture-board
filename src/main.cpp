0b4a08cc042c4ae0ee16f42651581fda5abdef86
Ana <33756496+Anakin2012@users.noreply.github.com>
2019-01-05 23:15:32 +0100

    Deleted unnecessary functions

--- src/main.cpp
+++ src/main.cpp
@@ -91,11 +91,13 @@ main(int argc, char* argv[]) {
 	  {
 	  	 cg.updateGestureData();
 	  	 cg.parseData();
-	  	     //Display_Values();
+	  	 //Display_Values();
 	  }
 
 	  uint8_t i = 0;
-	/*
+
+	  // PROVERA
+	  /*
 	  while(1)
 	  {
 		  for(i = 0; i < 17; i++)
@@ -149,13 +151,6 @@ void gestureDisplayValues() {
     	{
     		flag=1;
 
-        if(touch_evt & TouchSouth) trace_printf("touch south\r\n");
-        if(touch_evt & TouchWest) trace_printf("touch west\r\n");
-        if(touch_evt & TouchNorth) trace_printf("touch north\r\n");
-        if(touch_evt & TouchEast) trace_printf("touch east\r\n");
-        //if(touch_evt & TouchSouth) TFT_WRITE_TEXT("TOUCH SOUTH",120,90);
-        if(touch_evt & TouchCenter) trace_printf("touch center\r\n");
-
     		if(touch_evt & TapSouth)  uartSendGestureData(7);     //trace_printf("tap south\r\n");
     		if(touch_evt & TapWest)   uartSendGestureData(9); 	  //trace_printf("tap west\r\n");
     		if(touch_evt & TapNorth)  uartSendGestureData(6);     //trace_printf("tap north\r\n");
@@ -200,128 +195,6 @@ void gestureDisplayValues() {
 }
 */
 
-
-void Display_Values() {
-
-	//trace_printf("%u \r\n",mGesture.DataOut.Position.X);
-
-    //Touch Info
-    touch_evt = (unsigned int) mGesture.DataOut.TouchInfo & 0xFFFF;
-    if ((touch_evt != 0) && (touch_evt != touch_evt_old)) {
-      //TFT_Rectangle(80,60,320,240);
-      if(touch_evt < 0x00000201) {
-        flag=1;
-
-        if(touch_evt & TouchSouth) trace_printf("touch south\r\n");
-        if(touch_evt & TouchWest) trace_printf("touch west\r\n");
-        if(touch_evt & TouchNorth) trace_printf("touch north\r\n");
-        if(touch_evt & TouchEast) trace_printf("touch east\r\n");
-        //if(touch_evt & TouchSouth) TFT_WRITE_TEXT("TOUCH SOUTH",120,90);
-        if(touch_evt & TouchCenter) trace_printf("touch center\r\n");
-
-        if(touch_evt & TapSouth) trace_printf("tap south\r\n");
-        if(touch_evt & TapWest) trace_printf("tap west\r\n");
-        if(touch_evt & TapNorth) trace_printf("tap north\r\n");
-        if(touch_evt & TapEast) trace_printf("tap east\r\n");
-        if(touch_evt & TapCenter) trace_printf("tap center\r\n");
-
-      }else{
-        if(touch_evt & DoubleTapSouth) trace_printf("double tap south\r\n");
-        if(touch_evt & DoubleTapWest) trace_printf("double tap west\r\n");
-        if(touch_evt & DoubleTapNorth) trace_printf("double tap north\r\n");
-        if(touch_evt & DoubleTapEast) trace_printf("double tap east\r\n");
-        if(touch_evt & DoubleTapCenter) trace_printf("double tap center\r\n");
-      }
-      touch_evt_old = touch_evt;
-    }
-
-
-    //Gesture info
-    gest_evt = (char) mGesture.DataOut.GestureInfo & 0xFF;
-    gest_class = (char) (mGesture.DataOut.GestureInfo >> 12) & 0x0F;
-    gest_edge_flick = (char) (mGesture.DataOut.GestureInfo >> 16) & 0x01;
-    gest_recognition = (char) (mGesture.DataOut.GestureInfo >> 31) & 0x01;
-
-      if (gest_evt) {
-        flag=2;
-          switch (gest_evt) {
-            case 1:
-
-              trace_printf("GARBAGE\r\n");
-              break;
-            case 2:
-
-              trace_printf("flick left to right\r\n");
-              break;
-            case 3:
-
-             trace_printf("flick right to left\r\n");
-              break;
-            case 4:
-
-              trace_printf("flick up\r\n");
-              break;
-            case 5:
-              trace_printf("flick down\r\n");
-              break;
-          }
-
-      }
-
-
-    //Air Wheel info
-     if (mGesture.DataOut.AirWheelInfo !=0) {
-      if (flag!=3) {                                                                     // check if the air wheel has been toggled the first time
-                                                            // if so, display the circle image on TFT
-       if (mGesture.DataOut.AirWheelInfo > OLDAirWheelInfo)                              // if the airwheel counter increments,
-       {
-    	 clk=1;
-         trace_printf("Clockwise\r\n");
-		                                            // it is a clockwise rotation
-         }
-       else if  (mGesture.DataOut.AirWheelInfo < OLDAirWheelInfo)                       // if it decrements,
-       {
-    	  cclk=1;
-
-    	  trace_printf("Counter-Clockwise\r\n");
-		                                   // it is counter-clokwise
-       }
-                                                    // print out the airwheel counter info
-         flag=3;
-       }
-
-       else if (flag==3) {                                                            // if the previous gesture is already the airwheel
-        if ((clk==1) & (mGesture.DataOut.AirWheelInfo > OLDAirWheelInfo))             // if the rotation was clockwise and now is incrementing,
-        {
-
-        }
-
-        else if  ((clk==1) & (mGesture.DataOut.AirWheelInfo < OLDAirWheelInfo)) {    // if the rotation was clockwise and now is decrementing,
-          cclk=1;                                                                    // print out counter-clokwise, and update airwheel info
-          clk=0;
-
-          trace_printf("counter-clockwise\r\n");
-        }
-
-        else if ((cclk==1) & (mGesture.DataOut.AirWheelInfo < OLDAirWheelInfo)) {   //if the rotation was counter-clokwise and now is decrementing,
-
-        }
-
-        else if ((cclk==1) & (mGesture.DataOut.AirWheelInfo > OLDAirWheelInfo)) {   //if the rotation was counter-clockwise and now is incrementing,
-          cclk=0;                                                                   // print out clockwise, and update airwheel counter info
-          clk=1;
-
-         trace_printf("Clockwise\r\n");
-
-        }
-       }
-
-       OLDAirWheelInfo=mGesture.DataOut.AirWheelInfo;
-     }
-    HAL_Delay(50);
-
-
-}
 
 
 #pragma GCC diagnostic pop
